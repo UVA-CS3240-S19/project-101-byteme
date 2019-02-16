7f5e59808a25fe257d2b12d9bd0b6360288eb161
Michael Chang <chang.michael1009@gmail.com>
2019-02-15 22:08:26 -0500

    Reorganizing files to have manage.py at root of git repo

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {
+        
+    }
+    return render(request, 'app/main_page.html', context)
+
+
+def sign_up(request):
+    context = {}
+    return render(request, 'app/sign_up.html', context)
+
+
+def profile(request):
+    context = {}
+    return render(request, 'app/profile.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'app/login_page.html', context)
