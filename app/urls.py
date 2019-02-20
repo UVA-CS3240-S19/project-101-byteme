5f26fc645c3a9078b6874afb8e5a3856b2e1a645
Kathryn Wason <klw8fg@virginia.edu>
2019-02-20 13:38:54 -0500

    linking nav bar to pages

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     #path('profile/', views.create_profile, name='profile'),
     path('profile/', views.create_profile, name='profile'),
     path('published_profile/', views.ProfileView.as_view(), name='published_profile'),
-    path('news_feed/', views.news_feed, name='feed'),
+    path('news_feed/', views.news_feed, name='news_feed'),
+    path('messaging/', views.messaging, name='messaging'),
+    path('friends/', views.friends, name='friends'),
+    path('notifications/', views.notifications, name='notifications'),
+    path('settings/', views.settings, name='settings'),
+
 ]
--- skimatch/skimatch/urls.py
+++ skimatch/skimatch/urls.py
@@ -21,5 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'main_page'),
     path('sign_up/', views.sign_up, name = 'sign_up'),
-    path('profile/', views.profile, name = 'profile')
+    path('profile/', views.create_profile, name = 'profile'),
+    path('published_profile/', views.ProfileView.as_view(), name='published_profile'),
+    path('news_feed/', views.news_feed, name='news_feed'),
+    path('messaging/', views.messaging, name='messaging'),
+    path('friends/', views.friends, name='friends'),
+    path('notifications/', views.notifications, name='notifications'),
+    path('settings/', views.settings, name='settings'),
 ]
