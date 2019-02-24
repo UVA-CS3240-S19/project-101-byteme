856ae47fd42c037f635abd62c071342b7e6c4301
jelenaliu <jl2gb@virginia.edu>
2019-02-24 16:33:47 -0500

    develop

--- app/models.py
+++ app/models.py
@@ -1,20 +1,3 @@
 from django.db import models
-from django.forms import ModelForm
 
-class Profile(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.CharField(max_length=5)
-    major = models.CharField(max_length=200)
-    bio = models.TextField(max_length=600)
-    skills = models.CharField(max_length=100)
-    courses = models.CharField(max_length=200)      # eventually drop down menu? hashtags?
-    organizations = models.CharField(max_length=200)
-    interests = models.CharField(max_length=100)
-    # picture = models.ImageField()
-
-
-class ProfileModel(ModelForm):
-    model = Profile
-    fields = ['name', 'year', 'major', 'bio', 'skills', 'courses','organizations', 'interests']
-    # waiting to add picture for now
-
+# Create your models here.
--- app/views.py
+++ app/views.py
@@ -1,80 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
-from django.http import HttpResponseRedirect
-from django.forms import modelform_factory
-from django.views import generic
-from django.urls import reverse
+from django.shortcuts import render, redirect
 
-from .models import Profile, ProfileModel
 
-# published profile view
-class ProfileView(generic.DetailView):
-    template_name = 'app/published_profile.html'
-    context_object_name = 'profile'
-
-    def get_queryset(self):
-        return Profile.objects.all()
-
-# form to create profile
-def create_profile(request):
-    ProfileModel = modelform_factory(Profile, fields=('name', 'year', 'major', 'bio', 'skills', 'courses','organizations', 'interests'))
-    if request.method == "POST":
-        form = ProfileModel(request.POST)
-        if (form.is_valid()):
-            profile = form.save()
-            return HttpResponseRedirect(reverse('app:published_profile', args=(profile.id,)))
-        else:
-            return render(request, 'app/profile.html', {'form': ProfileModel()})
-
-    else:
-        return render(request, 'app/profile.html', {'form': ProfileModel()})
-
-posts = [
-    {
-        'author': 'Student',
-        'title': 'Post 1',
-        'content': 'First post',
-        'date_posted': 'February 20, 2019'
-    }
-]
-
-def news_feed(request):
+def home(request):
     context = {
-        'posts': posts
+        
     }
+    if not request.user.is_authenticated:
+        return redirect('login') 
+    else:
+        return render(request, 'app/main_page.html', context)
 
-    return render(request, 'app/news_feed.html', context)
 
-def messaging(request):
-    return render(request, 'app/messaging.html')
+def sign_up(request):
+    context = {}
+    return render(request, 'app/sign_up.html', context)
 
-def notifications(request):
-    return render(request, 'app/notifications.html')
 
-def friends(request):
-    return render(request, 'app/friends.html')
+def profile(request):
+    context = {}
+    if not request.user.is_authenticated:
+        return redirect('login') 
+    else:
+        return render(request, 'app/profile.html', context)
 
-def settings(request):
-    return render(request, 'app/settings.html')
+def login(request):
+    context = {}
+    return render(request, 'app/login_page.html', context)
 
-# def home(request):
-#     context = {
-#
-#     }
-#     return render(request, 'app/main_page.html', context)
-#
-#
-# def sign_up(request):
-#     context = {}
-#     return render(request, 'app/sign_up.html', context)
-#
-#
-# def profile(request):
-#     context = {}
-#     return render(request, 'app/profile.html', context)
-#
-#
-# def login(request):
-#     context = {}
-#     return render(request, 'app/login_page.html', context)
