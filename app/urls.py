a6742252d26dee8a4a1f8901cfc789fe1223e355
jelenaliu <jl2gb@virginia.edu>
2019-03-01 23:33:35 -0500

    Revert ".gitignore"
    This reverts commit 7e51997962f1abcae05690e0d9d8997c81bf2c31.

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,7 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     #path('profile/', views.create_profile, name='profile'),
-    path('published_profile/<int:pk>', views.ProfileView.as_view(), name='published_profile'),
-    path('update_profile/<int:pk>', views.update_profile, name='update_profile'),
+    path('published_profile/<int:pk>/', views.ProfileView.as_view(), name='published_profile'),
     path('news_feed/', views.news_feed, name='news_feed'),
     path('messaging/', views.messaging, name='messaging'),
     path('friends/', views.friends, name='friends'),
