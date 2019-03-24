f0156723da3bf61e88995929c22dc77199d95b81
slslotter <sls6fn@virginia.edu>
2019-03-24 00:00:31 -0400

    pulling repo

--- skimatch/settings.py
+++ skimatch/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
